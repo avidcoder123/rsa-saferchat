e820e34f4f3d31be747d113d1115cb7f78216aa7
avidcoder123 <umar.tli.edu@gmail.com>
2023-10-16 17:26:45 +0000

    save

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,12 @@
-<script>
+<script lang="ts">
     import { goto } from "$app/navigation";
     import { db } from "$lib/firebase";
     import { privateKeyStore, publicKeyStore, userIdStore } from "$lib/stores"
-    import { child, get, ref } from "firebase/database";
+    import { child, get, push, ref, set, update } from "firebase/database";
     import { get as getVal } from "svelte/store"
+
+    let ownID = getVal(userIdStore)
+
     if(getVal(publicKeyStore).length == 0) {
         goto("/")
     }
@@ -13,14 +16,37 @@
     async function chatRequest() {
         let receiver = await get(child(ref(db), `users/${receiverID}`)).then(x => x.val())
         if(!receiver) return alert("Invalid Receiver ID. Check your spelling.")
+        //Encrypt the hello message
+        let encrypted = cryptico.encrypt(initMsg, receiver.publicKey, getVal(privateKeyStore)).cipher!
+        let chatname = ""
+        //Put ownerIDs in alphabetical order
+        if(ownID < receiverID) {
+            chatname = ownID + receiverID
+        } else {
+            chatname = receiverID + ownID
+        }
+        //Create initial message
+        await set(ref(db, `/chat/${chatname}`), {
+            member1: ownID,
+            member2: receiverID,
+            chat2: encrypted //Send message to member 2
+        })
+        //Add chat to each user's list
+        await set(push(ref(db, `users/${ownID}/chats`)), {id: chatname})
+        await set(push(ref(db, `users/${receiverID}/chats`)), {id: chatname})
     }
+
+    let userChats = get(child(ref(db), `user/${ownID}/chats`))
 </script>
 <div class="p-5 flex flex-col gap-3">
-    <h1 class="text-white text-4xl">Your User ID is {getVal(userIdStore)}</h1>
+    <h1 class="text-white text-4xl">Your User ID is {ownID}</h1>
     <hr>
     <h1 class="text-white text-3xl">Send a chat request</h1>
     <input class="h-10 rounded-md border-white border-2 w-72 p-1" bind:value={receiverID} placeholder="Receiver ID">
     <textarea class="h-52 rounded-md border-white border-2 w-72 p-1" bind:value={initMsg} placeholder="Initial Message"></textarea>
     <button class="h-10 w-52 bg-blue-600 rounded-md p-1 text-white" on:click={chatRequest}>Send Chat Request</button>
     <hr>
+    {#await userChats then chatList}
+        {JSON.stringify(chatList)}
+    {/await}
 </div>
