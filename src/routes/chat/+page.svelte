4a6cde3b9d47dcdac72a1ce1c036f078446f076a
avidcoder123 <umar.tli.edu@gmail.com>
2023-10-23 15:35:36 +0000

    finish

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -2,7 +2,7 @@
     import { goto } from "$app/navigation";
     import { db } from "$lib/firebase";
     import { currentChatStore, privateKeyStore, publicKeyStore, userIdStore } from "$lib/stores";
-    import { child, get, ref } from "firebase/database";
+    import { child, get, onValue, ref, set } from "firebase/database";
     import { onMount } from "svelte";
     import { get as getVal } from "svelte/store";
 
@@ -43,16 +43,28 @@
     }).then(user => otherPublicKey = user.publicKey)
     .then(() => get(child(ref(db), "chats/" + chatid)).then(x => x.val()))
     .then(chat => {
-        if(chat) {
-            let encryptedMsg = chat["chat" + memberID] as string
-            let decryptedMsg = cryptico.decrypt(encryptedMsg, privateKey)
-            console.log(decryptedMsg)
+        // if(chat) {
+        //     let encryptedMsg = chat["chat" + memberID] as string
+        //     let decryptedMsg = cryptico.decrypt(encryptedMsg, privateKey)
+        //     console.log(decryptedMsg)
+        //     chatList = [...chatList, ({
+        //         sender: memberID == 1 ? 2 : 1,
+        //         text: decodeURI(decryptedMsg.plaintext),
+        //         verified: decryptedMsg.signature == "verified" && decryptedMsg.publicKeyString == otherPublicKey
+        //     })]
+        // }
+
+        onValue(ref(db, `chats/${chatid}/chat${memberID}`), x => {
+            let msg = x.val()
+            let decryptedMsg = cryptico.decrypt(msg, privateKey)
             chatList = [...chatList, ({
                 sender: memberID == 1 ? 2 : 1,
                 text: decodeURI(decryptedMsg.plaintext),
                 verified: decryptedMsg.signature == "verified" && decryptedMsg.publicKeyString == otherPublicKey
             })]
-        }
+            let msgbox = document.getElementById("scrollTo")!
+            msgbox.scrollIntoView();
+        })
     })
 
     onMount(() => {
@@ -69,36 +81,43 @@
     let chatText = "" 
 
     function sendMsg() {
+        if(!chatText) return
         chatList = [...chatList, {
             sender: memberID,
             text: chatText,
             verified: true
         }]
-        chatText = ""
-        //TODO: update in fbase
+        let cipher = cryptico.encrypt(encodeURI(chatText), otherPublicKey, privateKey).cipher!
+        set(ref(db, `chats/${chatid}/chat${memberID == 1 ? 2 : 1}`), cipher).then(() => chatText = "")
+        .then(() => {
+            let msgbox = document.getElementById("scrollTo")!
+            msgbox.scrollIntoView();
+        })
+
     }
 
 </script>
-{#each chatList as msg}
-<div class="flex flex-col">
-    <div class={"my-2 alert " + (msg.sender == memberID ? "alert-success" : "")}>
-        <div>
-          <h3 class="font-bold">
-            {msg.sender == memberID ? "You" : members[msg.sender - 1]}
-            {#if msg.sender != memberID}
-                {#if msg.verified}
-                    <div class="badge badge-success">Verified</div>
-                {:else}
-                    <div class="badge badge-error">Signature forged! The message may have been sent by a hacker!</div>
+<div class="flex flex-col gap-1 overflow-y-auto mb-12 pb-1" id="messageslist">
+    {#each chatList as msg}
+        <div class={"alert " + (msg.sender == memberID ? "alert-success" : "")}>
+            <div>
+            <h3 class="font-bold">
+                {msg.sender == memberID ? "You" : members[msg.sender - 1]}
+                {#if msg.sender != memberID}
+                    {#if msg.verified}
+                        <div class="badge badge-success">Verified</div>
+                    {:else}
+                        <div class="badge badge-error">Signature forged! The message may have been sent by a hacker!</div>
+                    {/if}
                 {/if}
-            {/if}
-        </h3>
-        <div class="text-lg">{msg.text}</div>
+            </h3>
+            <div class="text-lg">{msg.text}</div>
+            </div>
         </div>
-      </div>
+    {/each}
+    <span id="scrollTo"></span>
 </div>
-{/each}
-<div class="flex flex-row fixed bottom-0 w-full">
+<div class="flex flex-row fixed bottom-0 w-full h-12">
     <input class="w-[85%] h-12 pl-2" placeholder="Enter message" id="chatbox" bind:value={chatText} autofocus/>
     <button class="w-[15%] h-12 bg-success text-black text-lg" id="submit" on:click={sendMsg}>Send</button>
 </div>
